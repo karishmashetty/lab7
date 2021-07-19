e321e63e1ea056659662369d9c07f76f9f437e93
karishmashetty <83230615+karishmashetty@users.noreply.github.com>
2021-07-19 14:17:16 +0530

    Add files via upload

---
+++ e7.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="1">
+<title>DIGITal clock</title>
+<style>h1{
+    text-align: center;
+    color: yellowgreen;
+    margin-top: 100px;
+}
+</style>
+</head>
+<body>
+<?php
+echo "<h1>digital clock</h1>";
+echo "<hr>";
+ echo "<h1>".date('h:i:s A')."</h1>";
+?>
+</body></html>
